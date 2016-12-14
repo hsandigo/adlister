5d01a3a9309a7b8546c1e39e9244787c0cb8b7f1
Humberto Sandigo <sandigohumberto@gmail.com>
2016-12-14 16:16:59 -0600

    created login servlet and fixed login.jsp file

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,26 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by humbertosandigo on 12/14/16.
+ */
+
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        if (username.equals("admin") && password.equals("password")) {
+            resp.sendRedirect("/profile");
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/login.jsp").forward(request, response);
+    }
+}
