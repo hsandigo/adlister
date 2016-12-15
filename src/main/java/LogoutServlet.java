57cbfb6ff7a0ede3d82e3df630d80235dd53953f
Humberto Sandigo <sandigohumberto@gmail.com>
2016-12-15 11:51:49 -0600

    sessions and cookies exercise logout complete

--- src/main/java/LogoutServlet.java
+++ src/main/java/LogoutServlet.java
@@ -4,12 +4,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
-@WebServlet(name = "AdsIndexServlet", urlPatterns = "/ads")
-public class AdsIndexServlet extends HttpServlet {
+/**
+ * Created by humbertosandigo on 12/15/16.
+ */
+@WebServlet(name = "LogoutServlet", urlPatterns = "/logout")
+public class LogoutServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("ads", DaoFactory.getAdsDao().all());
-        request.getRequestDispatcher("/WEB-INF/ads/index.jsp").forward(request, response);
+        request.getSession().removeAttribute("user");
+        response.sendRedirect("/login");
     }
 }
